ede4877f5cdeef051d4e956733acc756c7a1517f
osla08001 <osla08001@E-PW04SMBR>
2025-10-24 13:40:21 +0200

    Add project files.

---
+++ 45/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace inl45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int x = Int32.Parse(input.Substring(i,1));
+               
+                if(x == 9)
+                {
+                    x = 0;
+                }
+                else
+                {
+                    x += 1;
+                }
+
+                    Console.Write(x);
+            }
+        }
+    }
+}
